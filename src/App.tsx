158212be35ba8853d97c3ee2ac8e01ba74209c96
Jakub Barszcz <jakub.barszcz33@gmail.com>
2025-03-11 22:12:22 +0100

    JB-2: fixing styles and creating variables.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import './styles/main.scss';
-import { Dashboard } from './pages/Dashboard';
 import { HomePage } from './pages/HomePage';
 
 function App() {
   return (
     <>
-      <Dashboard />
       <HomePage />
     </>
   );
